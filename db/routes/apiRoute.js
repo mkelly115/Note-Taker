be1442f2c103255fccad7f900c34d9c97f0f67a6
michael <mikek222@gmail.com>
2023-12-19 19:20:16 +0300

    completed post route, cleaned up code

--- db/routes/apiRoute.js
+++ db/routes/apiRoute.js
@@ -8,5 +8,20 @@ app.get('/api/notes',(req, res) => {
     res.sendFile(path.join(__dirname, 'db/db.json'));
 })
 
+app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db)
+
+    let userWrite = {
+        title: req.body.title,
+        text: req.text.title,
+        ID: uuid()
+    }
+
+    db.push(userWrite);
+    
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+});
 
 }
